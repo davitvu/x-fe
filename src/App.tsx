739eb3e3c46d7c1e272b262622b08e41adac4b90
davitvu <thomanhdiroinoi@gmail.com>
2025-05-30 23:35:47 +0700

    add intro loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import AppRoutes from "./routes/AppRoutes";
+import LoadingIntro from "./pages/Loading/LoadingIntro";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <LoadingIntro />;
 
   return (
     <ThemeProvider>
