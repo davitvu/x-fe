d97a75a6b711fdd0b7584463c7451a6cff6cdc12
davitvu <thomanhdiroinoi@gmail.com>
2025-06-05 19:55:15 +0700

    hẹ hẹ

--- src/pages/Auth/ModalAuth/Login.tsx
+++ src/pages/Auth/ModalAuth/Login.tsx
@@ -58,7 +58,19 @@ const Login = () => {
             <h1 className="self-start mb-6 text-2xl font-bold text-black sm:my-6 sm:text-3xl dark:text-white">Sign in to X</h1>
 
             {/* Google login button */}
-            <div
+            <div className="w-full md:w-[300px] h-[38px] flex items-center justify-center cursor-pointer border-[1px] hover:border-[#d5e3fa] transition-all duration-300 ease-in-out hover:bg-[#f1f6fe] border-[#dbdce0] rounded-[20px] mb-4 dark:bg-white dark:text-black ">
+                <svg className="w-[18px] h-[18px] mr-2" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                    <g>
+                        <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                        <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                        <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                        <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                        <path fill="none" d="M0 0h48v48H0z"></path>
+                    </g>
+                </svg>
+                <span className="text-md md:text-[15px] font-medium text-[rgb(60, 64, 67)]">Sign up with Google</span>
+            </div>
+            {/* <div
                 onClick={() => alert("tính năng chưa dùng được ăn em thong cãm")}
                 className="w-full md:w-[300px] px-2 h-[38px] sm:h-[40px] flex items-center justify-between cursor-pointer border-[1px] hover:border-[#d5e3fa] transition-all duration-300 ease-in-out hover:bg-[#f1f6fe] border-[#dbdce0] rounded-[20px] mb-5 dark:bg-white dark:text-black "
             >
@@ -78,7 +90,7 @@ const Login = () => {
                         <path fill="none" d="M0 0h48v48H0z"></path>
                     </g>
                 </svg>
-            </div>
+            </div> */}
             {/* Google login button */}
 
             {/* Apple login button */}
