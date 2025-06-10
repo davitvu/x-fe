a5e835cd1472acffa9fcf29f1506d5fdd1bd791d
davitvu <thomanhdiroinoi@gmail.com>
2025-06-10 20:35:08 +0700

    config CORS

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true
+    host: true,
+    proxy: {
+      '/api/v1/auth': {
+        target: 'http://localhost:5001',
+        // changeOrigin: true,
+        // secure: false,
+      }
+    }
   }
 })
