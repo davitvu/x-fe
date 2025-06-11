1be250148614054f8ae79d69141e79c016c48927
davitvu <thomanhdiroinoi@gmail.com>
2025-06-11 18:41:28 +0700

    tesst tests

--- src/services/axios.customize.ts
+++ src/services/axios.customize.ts
@@ -31,13 +31,18 @@ instance.interceptors.response.use(function (response) {
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+    // if (error) {
+    //     if (error.response?.status === 401) {
+    //         // localStorage.removeItem('access_token');
+    //         // window.location.href = '/auth';
+    //     }
+    // }
+    // return Promise.reject(error + "hahsdfihasifsda");
+
     if (error) {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('access_token');
-            // window.location.href = '/auth';
-        }
+        return error.response;
     }
-    return Promise.reject(error + "hahsdfihasifsda");
+    return Promise.reject(error);
 });
 
 export default instance;
